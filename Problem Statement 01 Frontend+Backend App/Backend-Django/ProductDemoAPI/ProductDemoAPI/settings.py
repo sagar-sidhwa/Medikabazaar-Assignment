c28759aece79e922bc93507865d551786a35025d
sagar-sidhwa <44087369+sagar-sidhwa@users.noreply.github.com>
2022-04-25 18:09:43 +0530

    Update settings.py

--- Problem Statement 01 Frontend+Backend App/Backend-Django/ProductDemoAPI/ProductDemoAPI/settings.py
+++ Problem Statement 01 Frontend+Backend App/Backend-Django/ProductDemoAPI/ProductDemoAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g(mmul57lxq=@q%)g@k=pajdy&px=&@7$+*@$a!qbs!mm&i40v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
